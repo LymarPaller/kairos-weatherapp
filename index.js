a7fd0ce4b04179cf640a004f6e55005e464bf531
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-07-11 13:06:56 +0800

    created: basic div structures

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // const forecastAPI = `https://api.weatherapi.com/v1/forecast.json?key=${forecastAPIKEY}%20&q=${cityName}&days=6&aqi=no&alerts=no`;
 
-// const geoLocAPI = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
+// const geoLocAPI = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={geoLocAPIKEY}`
 
-const geoLocAPIKEY = 'yourAPIKEY';
-const forecastAPIKEY = 'yourAPIKEY';
+const geoLocAPIKEY = '${geoLocAPIKEY1}${geoLocAPIKEY2}';
+const forecastAPIKEY = '${forecastAPIKEY1}${forecastAPIKEY2}';
+
+const forecastAPIKEY1 = 'ad6a22d37a6e4fdeb';
+const forecastAPIKEY2 = '45135830230707';
+const geoLocAPIKEY1 = '42207e457e3eb8b6d';
+const geoLocAPIKEY2 = 'f3dd8146f5bfc1b';
 
 // GETTING CITY NAME VIA SEARCH BOX not yet implemented FOR TESTING API
 // const getCityName = () => {
